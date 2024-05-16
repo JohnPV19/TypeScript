4312f2a15afc80df788ba12e9d53c222ba95efe2
João Valente <joao.valente5935@gmail.com>
2024-05-16 21:55:28 +0100

    update

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -2,8 +2,12 @@ let tax:number | string = 10;
 tax = 100;
 tax = '$10';
 
+let banana:number| string = 10;
+banana= 20;
+banana = '22';
 
 let requestStatus: 'pending' | 'success' | 'error' = 'pending' ;
 requestStatus = 'success';
 requestStatus = 'error';
  console.log(tax)
+ console.log(banana)
