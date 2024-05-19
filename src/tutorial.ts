cab5d136dcd19ff078d0d51e4680615e47e66dbf
João Valente <joao.valente5935@gmail.com>
2024-05-19 16:01:10 +0100

    testing

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -5,10 +5,13 @@ tax = '$10';
 let banana:number| string = 10;
 banana= 20;
 banana = '22';
-banana = '25';
+banana = '25git ';
 
 let requestStatus: 'pending' | 'success' | 'error' = 'pending' ;
 requestStatus = 'success';
 requestStatus = 'error';
  console.log(tax)
  console.log(banana)
+
+
+ // testing
